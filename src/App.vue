2859440d8b091756bbccfee53912cd3885efaaa2
HermersonDev <ne.hermerson@gmail.com>
2019-11-06 09:09:22 -0300

    vue v-model.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 	<!--  <layout id="app" /> -->
 
 	<!-- <a v-bind:href="link" target="_blank">{{ message }}</a> -->
+	
 	<div>
+		
 		<!-- <div v-if="display"> Exibindo a primeira opção</div>
 		<div v-else>Outra opção no caso contrário</div> -->
 		<h1>Lista de Compra</h1>
@@ -11,7 +13,14 @@
 			<li v-for="item in products" :key="item">{{ item }}</li>
 		</ul>
 
-		<button class="btn btn-primary">Inserir</button>
+	<!-- 	<button class="btn btn-success" v-on:click="insert">
+			Inserir
+		</button> -->
+		<input type="text" name="" v-model="newItem">
+		<button class="btn btn-success" @click="insert" >
+			Inserir
+		</button>
+
 	</div>
 
 </template>
@@ -28,11 +37,12 @@
 		},
 
 		created() {
-			setTimeout(() => {
-				/*this.link = 'https://expotec2019.com.br';*/
+			/*setTimeout(() => {
+				this.link = 'https://expotec2019.com.br';
 				this.display = false;
 
-			}, 6000);
+			}, 6000);*/
+
 		},
 
 		data: () => {
@@ -40,9 +50,19 @@
 				// message: 'Hello World',
 				// link: 'https://google.com',
 				// display: true,
-				products: ['Arroz', 'Feijão', 'Suco Fruta']
+				products: ['Arroz', 'Feijão', 'Suco Fruta'],
+				newItem: ''
 			};
+		},
+
+		methods: {
+			insert() {
+				this.products.push(this.newItem);
+				this.newItem = '';
+
+			}
 		}
+
 	}
 
 </script>
