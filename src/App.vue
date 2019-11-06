e383e9eb10521074ff8a178cddea5fc2e2accd6d
HermersonDev <ne.hermerson@gmail.com>
2019-11-06 08:41:01 -0300

    vue if e else

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
-    <layout id="app" />
+
+	<!--  <layout id="app" /> -->
+
+	<!-- <a v-bind:href="link" target="_blank">{{ message }}</a> -->
+	<div>
+		<div v-if="display"> Exibindo a primeira opção</div>
+		<div v-else>Outra opção no caso contrário</div>
+	</div>
+
 </template>
 
+
+
 <script>
-import Layout from './layouts/Layout.vue';
-export default {
-  name:'App',
-  components: {
-    Layout
-  }
-}
+
+	/*import Layout from './layouts/Layout.vue';*/
+	export default {
+		name:'App',
+		components: {
+			/*Layout*/
+		},
+
+		created() {
+			setTimeout(() => {
+				/*this.link = 'https://expotec2019.com.br';*/
+				this.display = false;
+
+			}, 6000);
+		},
+
+		data: () => {
+			return {
+				message: 'Hello World',
+				link: 'https://google.com',
+				display: true
+			};
+		}
+	}
+
 </script>
